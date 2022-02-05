8e41e1f87e8cd7a7195c3d72523fc08e5fa5cd4c
stunnel478 <95111099+stunnel478@users.noreply.github.com>
2022-02-05 11:37:52 +0700

    Update ssr.sh

--- ssr/ssr.sh
+++ ssr/ssr.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/DhedyKurnia/vps/master/ipvps.txt | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/stunnel478/vps/master/ipvps.txt | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
@@ -24,7 +24,7 @@ echo -e "${NC}${LIGHT}Telegram : https://t.me/zeaking5"
 exit 0
 fi
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/DhedyKurnia/vps/master/ssr"
+akbarvpn="raw.githubusercontent.com/stunnel478/vps/master/ssr"
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
