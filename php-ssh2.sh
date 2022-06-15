c9fb015cd5b94bc93897a805c54b6ed0299a0e7b
stunnel478 <95111099+stunnel478@users.noreply.github.com>
2022-06-15 22:09:55 +0700

    Update php-ssh2.sh

--- php-ssh2.sh
+++ php-ssh2.sh
@@ -17,11 +17,11 @@ cd ..
 rm -r master.zip pecl-networking-ssh2-master
 
 echo "Adding ssh2 module config file"
-cat > /etc/php/7.4/fpm/conf.d/20-ssh2.ini << EOF
+cat > /etc/php/7.2/fpm/conf.d/20-ssh2.ini << EOF
 ; configuration for php ssh2 module
 ; priority=20
 extension=ssh2.so
 EOF
 
 echo "Restarting PHP 7 fpm"
-service php7.4-fpm restart &>/dev/null
+service php7.2-fpm restart &>/dev/null
